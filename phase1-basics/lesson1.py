8e301d93455fd3e6306348b30d7311c16ca1d55a
Fahad00888 <khalidfahad630@gmail.com>
2026-04-13 13:45:33 +0500

    Add lesson 1 - first Python program

---
+++ phase1-basics/lesson1.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+colour = input("What is your favorite color? ")
+age = input("What is your age? ")
+
+print(f"Hello {name}! You are {age} years old and your favorite color is {colour}. You will be a great Python developer!")
